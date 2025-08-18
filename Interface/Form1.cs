48a5779acb6a185ad2caa75100aba0d24fd0db1d
Gabriel LabesC:Python27python.exe <gabriel@lermensys.com.br>
2025-08-18 18:55:01 -0300

    Adição

--- Interface/Form1.cs
+++ Interface/Form1.cs
@@ -21,5 +21,31 @@ namespace Interface
         {
 
         }
+
+        private void richTextBox1_TextChanged(object sender, EventArgs e)
+        {
+            if(e is KeyEventArgs chave)
+            {
+                Console.WriteLine(chave.KeyCode);
+            }
+        }
+
+        private void entradaTecladoProgama(object sender, KeyEventArgs e)
+        {
+            // Aqui você pode adicionar lógica para lidar com eventos de teclado
+            // Por exemplo, verificar se uma tecla específica foi pressionada
+            if (e.KeyCode == Keys.Enter)
+            {
+                // Ação a ser executada quando a tecla Enter for pressionada
+                int linha = int.Parse(this.richTextBox2.Text.Split('\n').Last());
+                linha++;
+                this.richTextBox2.AppendText("\n" + linha + "");
+
+            }
+            else
+            {
+                // Outras ações para outras teclas, se necessário
+            }
+        }
     }
 }
